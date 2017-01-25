4a25d3ae64db5590b538a3e90bc30ca062720bd2
shalldie <394229785@qq.com>
2017-01-25 13:22:40 +0800

    reset eslint

--- react/src/app.jsx
+++ react/src/app.jsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 import HelloWorld from './helloworld/HelloWorld';
 
+let args = {
+    name: 'tom'
+};
+
 ReactDOM.render(
-    <HelloWorld />,
+    <HelloWorld {...args} />,
     document.getElementById('root')
 );
