29fb0abc690c7e29329988619a52ce60ed4da6cb
谢爽 <394229785@qq.com>
2017-04-07 02:18:58 +0800

    添加自动识别系统打开浏览器

--- react/server.js
+++ react/server.js
@@ -5,17 +5,20 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
+let domain = '127.0.0.1';
+let port = 3000;
+
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, domain, function (err, result) {
     if (err) {
         return console.log(err);
     }
 
-    console.log('Listening at http://localhost:3000/')
+    let key = os.platform() == 'darwin' ? 'open' : 'explorer';
 
-    child.exec('explorer http://localhost:3000/');
+    child.exec(`${key} http://${domain}:${port}/`);
 
 });
